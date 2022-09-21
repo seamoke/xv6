4a04a5444903bc8c380a9fa97b64ea244ceb587e
seamoke <965605311@qq.com>
2022-09-21 20:40:25 +0800

    lazy allocation

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,6 +67,23 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
+  }
+    else if(r_scause()==13||r_scause()==15){
+      uint64 va = r_stval();
+      uint64 bottom = PGROUNDDOWN(va);
+      printf("%d\n",r_scause());
+      printf("page fault %p\n",va);
+      char* mem = kalloc();
+      if(mem == 0){
+        p->killed=1;
+      }
+      else{
+        memset(mem, 0, PGSIZE);
+        if(mappages(p->pagetable, bottom, PGSIZE, (uint64)mem, PTE_W|PTE_U|PTE_R) != 0){
+          kfree(mem);
+          p->killed=1;
+        }
+      }
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
