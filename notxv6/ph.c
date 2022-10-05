58186f37ac56daab62f0412f303a549a5a5b74c7
seamoke <965605311@qq.com>
2022-10-05 16:01:49 +0800

    mulit-thread

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,6 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+pthread_mutex_t lock[NBUCKET];
 
 double
 now()
@@ -28,7 +29,7 @@ now()
 static void 
 insert(int key, int value, struct entry **p, struct entry *n)
 {
-  struct entry *e = malloc(sizeof(struct entry));
+  struct entry *e = (struct entry *)malloc(sizeof(struct entry));
   e->key = key;
   e->value = value;
   e->next = n;
@@ -51,7 +52,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&lock[i]);
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&lock[i]);
   }
 }
 
@@ -102,13 +105,15 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
-
+  for(int i=0;i<NBUCKET;i++){
+    pthread_mutex_init(&lock[i],NULL);
+  }
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
     exit(-1);
   }
   nthread = atoi(argv[1]);
-  tha = malloc(sizeof(pthread_t) * nthread);
+  tha = (pthread_t *)malloc(sizeof(pthread_t) * nthread);
   srandom(0);
   assert(NKEYS % nthread == 0);
   for (int i = 0; i < NKEYS; i++) {
